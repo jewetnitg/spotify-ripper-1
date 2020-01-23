ec3a9ea19c9d4c686c1d4f1a0d7a800727d15394
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2020-01-23 20:32:03 +0100

    remove calls to termios to fix issues with running from nodejs

--- spotify_ripper/main.py
+++ spotify_ripper/main.py
@@ -15,7 +15,6 @@ import schedule
 import signal
 import select
 import tty
-import termios
 
 if sys.version_info >= (3, 0):
     import configparser as ConfigParser
@@ -561,26 +560,15 @@ def main(prog_args=sys.argv[1:]):
         abort(set_logged_in=True)
 
     # wait for ripping thread to finish
-    stdin_settings = termios.tcgetattr(sys.stdin)
     try:
-        tty.setcbreak(sys.stdin.fileno())
-
         while ripper.isAlive():
             schedule.run_pending()
-
-            # check if the escape button was pressed
-            if hasStdinData():
-                c = sys.stdin.read(1)
-                if c == '\x1b':
-                    skip()
             ripper.join(0.1)
     except (KeyboardInterrupt, Exception) as e:
         if not isinstance(e, KeyboardInterrupt):
             print(str(e))
         print("\n" + Fore.RED + "Aborting..." + Fore.RESET)
         abort()
-    finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, stdin_settings)
 
 if __name__ == '__main__':
     main()
