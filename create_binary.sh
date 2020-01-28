56e4ea42c942ff20b02a5404558adbc85e4c1ce3
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2020-01-28 18:24:51 +0100

    ensure binary name is 'spotifyripper'

--- create_binary.sh
+++ create_binary.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -rf *.spec dist build
-pyinstaller spotify_ripper/main.py --onefile --hidden-import=_cffi_backend --name spotify-ripper
+pyinstaller spotify_ripper/main.py --onefile --hidden-import=_cffi_backend --name spotifyripper
 
